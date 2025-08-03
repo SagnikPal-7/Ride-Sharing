a2d49a9f0dda1e28450e7d99548a21ac93ff2cd6
Sagnik Pal <131534396+SagnikPal-7@users.noreply.github.com>
2025-08-03 17:09:02 +0530

    speedInsights

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -14,6 +14,7 @@ import CaptainLogout from "./pages/CaptainLogout";
 import Riding from "./pages/Riding";
 import Profile from "./pages/Profile";
 import CaptainRiding from "./pages/CaptainRiding";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const App = () => {
   return (
@@ -70,6 +71,7 @@ const App = () => {
 
         <Route path="/captain-riding" element={<CaptainRiding />} />
       </Routes>
+      <SpeedInsights />
     </div>
   );
 };
